e44c9ae8075afa6b8d3ebd8c807688a66ff4828b
MidnightHammer <midnighthammer66@gmail.com>
2026-08-10 13:26:37 -0400

    Update texture.cpp

--- src/texture.cpp
+++ src/texture.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2026 MidnightHammer-code
+ * This source code is licensed under the MIT license
+ * LICENSE file in the root directory of this source tree.
+ */
+
+
 #include "../include/HammerEngine/HammerEngine.h"
 #include "../lib/stb_truetype.h"
 #include "../lib/stb_image.h"
